1adb3b787b761fa9e597dc78895e03a642440386
Dallas <dallaskelleydice@gmail.com>
2019-09-13 15:47:22 -0500

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+
+(
+    function(){
+        let active = false;
+        const nav = document.getElementsByClassName('nav-list')[0];
+        document.getElementById('menu-btn').addEventListener('click', (e)=>{
+            e.preventDefault();
+            active = !active;
+            if(active){
+                nav.classList.add('expanded');
+            }
+            else{
+                nav.classList.remove('expanded');
+            }
+        });
+    }
+)();
+
+
